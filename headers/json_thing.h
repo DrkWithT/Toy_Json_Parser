d904343228616116f5bb424e5a99368c6321bca8
drkwitht <derektan3510@gmail.com>
2023-03-25 08:51:51 -0700

    Rename main json value.

--- headers/json_thing.h
+++ headers/json_thing.h
@@ -1,24 +1,24 @@
-#ifndef JSON_OBJ_H
-#define JSON_OBJ_H
+#ifndef JSON_THING_H
+#define JSON_THING_H
 
 #include "json_data.h"
 
-typedef struct json_object
+typedef struct json_thing
 {
     /* data */
     char *file_name;
     Property *root;
-} JsonObject;
+} JsonThing;
 
 /**
  * @brief Creates and initializes a JSON representation data structure.
  * @note The file_name param must be a statically allocated C-String!
  * @param file_name The name of the JSON file.
- * @return JsonObject*
+ * @return JsonThing*
  */
-JsonObject *JsonObject_Create(const char* file_name);
+JsonThing *JsonThing_Create(const char* file_name);
 
 // @note: I need to implement a recursive function to free up memory in nested properties... Ahhhhh!
-// void JsonObject_Destroy(JsonObject *self);
+void JsonThing_Destroy(JsonThing *self);
 
 #endif
