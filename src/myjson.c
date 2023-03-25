abc7dd0a6bf4a9bd46d66a36fe73f469af927584
drkwitht <derektan3510@gmail.com>
2023-03-25 11:36:09 -0700

    Fixed wrong ptr usages.

--- src/myjson.c
+++ src/myjson.c
@@ -12,29 +12,32 @@ int main(void)
 {
     // Test pure JSON Array:    
     JsonThing *array_thing = JsonThing_Create("nums", ARR, 4); // The last arg is extra for the array case: I pass it anyways!
+    Array *array_ref = (Array*)array_thing->root->data.chunk;
 
-    Array_Push(array_thing->root, ArrayItem_Int(1));
-    Array_Push(array_thing->root, ArrayItem_Int(2));
-    Array_Push(array_thing->root, ArrayItem_Int(4));
-    Array_Push(array_thing->root, ArrayItem_Int(8));
+    Array_Push(array_ref, ArrayItem_Int(1));
+    Array_Push(array_ref, ArrayItem_Int(2));
+    Array_Push(array_ref, ArrayItem_Int(4));
+    Array_Push(array_ref, ArrayItem_Int(8));
 
-    size_t item_count = Array_Length(array_thing->root);
+    size_t item_count = Array_Length(array_ref);
 
     // Test 1: length should be 4!
-    printf("nums.length = %i\n", item_count);
+    printf("nums.length = %zu\n", item_count);
 
     // Test 2: output should be "1 2 4 8".
     printf("nums: ");
 
     for (size_t i = 0; i < item_count; i++)
     {
-        printf("%i ", Array_Get(array_thing->root, i));
+        printf("%zu ", Array_Get(array_ref, i));
     }
 
     printf("\n");
     
     // Test 3: Destroy simple Array:
-    Array_Destroy(array_thing->root);
+    JsonThing_Destroy(array_thing);
+    free(array_thing);
+    array_thing = NULL;
 
     // Test pure JSON Object:
     // todo
