14f15f97d68d71274cff50548725be06a56dc8d4
drkwitht <derektan3510@gmail.com>
2023-03-26 13:19:05 -0700

    Added lexer code.

---
+++ headers/json_lex.h
@@ -0,0 +1,55 @@
+#ifndef JSON_LEX_H
+#define JSON_LEX_H
+
+#include "json_token.h"
+
+/// Limits:
+#define MAX_JSON_LEN 10000
+
+/// Helpers:
+
+inline int is_wspace(char c) { return c == ' ' || c == '\t' || c == '\n' || c =='\r'; }
+inline int is_digit(char c) { return c >= '0' && c <= '9'; }
+inline int match_punct(char c, char ch) { return c == ch; }
+
+/**
+ * @brief Reads an entire file (json) into a dynamic char buffer. Returns NULL on failure. Either failed allocation or a size too large (over MAX_JSON_LEN) will cause failure.
+ * 
+ * @param file_path The file path.
+ * @return char*
+ */
+char *read_file(const char *file_path, size_t *external_len);
+
+/// Lexer:
+
+typedef struct json_lex
+{
+    char special_null[5]; // cached "null" name for lexing
+    char *doc_buf;
+    size_t doc_pos;
+    size_t doc_end;
+} Lexer;
+
+/**
+ * @brief Creates and initializes a new Lexer with an internal buffer of the small JSON file.
+ * 
+ * @param file_path Constant C-String naming the file.
+ * @return Lexer*
+ */
+Lexer *Lexer_Create(const char *file_path);
+
+/**
+ * @brief Moves out the dynamic buffer (to the parser) after resetting Lexer data.
+ * 
+ * @param self
+ */
+char* Lexer_CleanUp(Lexer *self);
+
+void Lexer_Skip_WSpc(Lexer *self);
+Token *Lexer_Lex_Punct(Lexer *self, TokenType punct_kind);
+Token *Lexer_Lex_Str(Lexer *self);
+Token *Lexer_Lex_Num(Lexer *self);
+Token *Lexer_Lex_Null(Lexer *self);
+TokenVec *Lexer_Lex_All(Lexer *self);
+
+#endif
