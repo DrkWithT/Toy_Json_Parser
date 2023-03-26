884524a55c1f7f8f5a3039a0780e383f13e01424
drkwitht <derektan3510@gmail.com>
2023-03-26 13:17:34 -0700

    Added Token code.

---
+++ headers/json_token.h
@@ -0,0 +1,50 @@
+#ifndef JSON_TOKEN_H
+#define JSON_TOKEN_H
+
+#include <stdlib.h>
+#include <stdio.h>
+
+/// Enums:
+
+typedef enum token_type {
+    WSPACE,       // space, tab, newline, return
+    LBRACKET,
+    RBRACKET,
+    LCURLY,
+    RCURLY,
+    STRBODY,
+    COLON,
+    INT_LTRL,     // text of [0-9]+ pattern
+    FLT_LTRL,     // text of [0-9]*.[0-9]+ pattern
+    NULL_LTRL,    // null keyword
+    COMMA,
+    FILE_END,     // eof token
+    UNKNOWN       // unsupported thing
+} TokenType;
+
+/// Token & TokenVec:
+
+typedef struct json_token
+{
+    TokenType type;
+    size_t begin;
+    size_t span;
+} Token;
+
+Token *Token_Create(TokenType _type, size_t _begin, size_t _span); // tokens should be externally freed...
+char *Token_ToTxt(const Token *self, const char *src);
+
+typedef struct token_vec
+{
+    Token **data;
+    size_t count;
+    size_t capacity;
+} TokenVec;
+
+TokenVec *TokenVec_Create(size_t _capacity);
+void TokenVec_Destroy(TokenVec *self);
+void TokenVec_Grow(TokenVec *self);
+void TokenVec_Set(TokenVec *self, size_t idx, Token *item);
+const Token *TokenVec_At(TokenVec *self, size_t idx);
+
+#endif
---
+++ src/json_token.c
@@ -0,0 +1,134 @@
+/**
+ * @file json_token.c
+ * @author Derek Tan (DrkWithT at GitHub)
+ * @brief Implements Token and TokenVec.
+ * @date 2023-03-26
+ */
+
+#include "json_token.h"
+
+/// Token:
+
+Token *Token_Create(TokenType _type, size_t _begin, size_t _span)
+{
+    Token *result = malloc(sizeof(Token));
+
+    if (!result)
+        return result;
+    
+    result->type = _type;
+    result->begin = _begin;
+    result->span = _span;
+
+    return result;
+}
+
+char *Token_ToTxt(const Token *self, const char *src)
+{
+    size_t buf_pos = 0;
+    size_t src_pos = self->begin;
+    size_t real_sz = self->span + 1; // include space for null terminator
+
+    char *txt = malloc(sizeof(char) * real_sz);
+
+    if (!txt)
+        return txt;
+
+    for (size_t count = 0; count < real_sz - 1; count++)
+    {
+        txt[buf_pos] = src[src_pos];
+        buf_pos++;
+        src_pos++;
+    }
+
+    txt[buf_pos] = '\0';
+
+    return txt;
+}
+
+/// TokenVec:
+
+TokenVec *TokenVec_Create(size_t _capacity)
+{
+    TokenVec *result = malloc(sizeof(TokenVec));
+
+    if (!result)
+        return result;
+    
+    result->capacity = _capacity;
+    result->count = 0;
+    result->data = malloc(sizeof(Token*) * _capacity);
+
+    if (!result->data)
+    {
+        result->capacity = 0;
+    }
+    else
+    {
+        for (size_t i = 0; i < result->capacity; i++)
+            result->data[i] = NULL; // initialize empty token slots
+    }
+
+    return result;
+}
+
+void TokenVec_Destroy(TokenVec *self)
+{
+    if (!self->data)
+        return;
+    
+    // free all referenced tokens
+    for (size_t i = 0; i < self->capacity; i++)
+    {
+        if (!self->data[i])
+            continue;
+        
+        free(self->data[i]);
+    }
+    
+    // free emptied token buffer
+    free(self->data);
+    self->data = NULL;
+    self->capacity = 0;
+}
+
+void TokenVec_Grow(TokenVec *self)
+{
+    if (!self->data)
+        return;
+
+    size_t new_capacity = self->capacity << 1;
+    size_t old_end_pos = self->capacity;
+
+    Token **temp = realloc(self->data, (sizeof(Token*) * new_capacity));
+
+    if (!temp)
+        return;
+
+    self->data = temp;
+    self->capacity = new_capacity;
+    // slot usage count is unchanged!
+
+    // initialize new empty slots after the older ones...
+    for (size_t i = old_end_pos; i < new_capacity; i++)
+        self->data[i] = NULL;
+}
+
+void TokenVec_Set(TokenVec *self, size_t idx, Token *item)
+{
+    // only set an empty slot and update length if ok!
+    if (self->data + idx == NULL)
+    {
+        self->data[idx] = item;
+        self->count++;
+    }
+
+    // also reallocate this vector when the old slots are full...
+    if (self->count == self->capacity)
+        TokenVec_Grow(self);
+}
+
+const Token *TokenVec_At(TokenVec *self, size_t idx)
+{
+    return self->data + idx;
+}
