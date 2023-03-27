94d70bdbd9a737401cccfe053376084e23ebdea7
drkwitht <derektan3510@gmail.com>
2023-03-27 14:59:39 -0700

    Added value relation enum.

--- headers/json_types.h
+++ headers/json_types.h
@@ -16,4 +16,11 @@ typedef enum json_type {
     NUL
 } DataType;
 
+/// Specifies relation of a primitive value to something: may be to none, to property, to array.
+typedef enum json_prox {
+    TO_NONE,  // primitive value is sole JSON data
+    TO_PROP,  // primitive value is object property value
+    TO_ARR    // primitive value is array item value 
+} JsonProx;
+
 #endif
