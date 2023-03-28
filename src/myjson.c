51152ee33932c09bf0ce8f4f23d38ae34d8535c9
drkwitht <derektan3510@gmail.com>
2023-03-27 19:12:27 -0700

    Replaced test code again.

--- src/myjson.c
+++ src/myjson.c
@@ -6,7 +6,7 @@
  */
 
 // #include "json_thing.h" // uncomment after parser is done
-#include "json_lex.h"
+#include "json_parser.h"
 #include <stdio.h>
 
 // Token typenames:
@@ -62,17 +62,45 @@ int main(int argc, char *argv[])
     for (size_t i = 0; i < slot_count; i++)
         Print_Token(i, TokenVec_At(tokens, i));
 
-    // free all dynamic memory:
-    TokenVec_Destroy(tokens);
-    free(tokens);
-    tokens = NULL;
+    // test Parser!
+    // DataType target_type = UNSUPPORTED;
+    // char *old_buf_ref = Lexer_CleanUp(lexer_ref);
+    // Parser *parser_ref = Parser_Create(old_buf_ref, tokens);
+    // JsonThing *json_ds = Parser_Start_Parse(parser_ref); 
 
+    // if (json_ds != NULL)
+    // {
+    //     if (strcmp(argv[1], "test1.json") == 0)
+    //     {
+    //         Array *arr = (Array*)json_ds->root->data.chunk;
+    //         printf("json_ds[0] = %i\n", Array_Get(arr, 0)->data.i);
+    //     }
+    //     else if (strcmp(argv[1], "test2.json") == 0)
+    //     {
+    //         Object *obj = (Object*)json_ds->root->data.chunk;
+    //         Property *clubs = (Property*)Property_AsChunk(Object_GetItem(obj, "clubs"), &target_type);
+    //         printf("json_ds[\"clubs\"][0] = %s\n", Array_Get((Array*)clubs->data.chunk, 0)->data.str);
+    //     }
+    // }
+
+    // free all dynamic memory:
     char *old_buf_ref = Lexer_CleanUp(lexer_ref);
     free(old_buf_ref);
     old_buf_ref = NULL;
 
+    TokenVec_Destroy(tokens);
+    free(tokens);
+    tokens = NULL;
+
     free(lexer_ref);
     lexer_ref = NULL;
+
+    // free(parser_ref);
+    // parser_ref = NULL;
+
+    // JsonThing_Destroy(json_ds);
+    // free(json_ds);
+    // json_ds = NULL;
 
     return 0;
 }
